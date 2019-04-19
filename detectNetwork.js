03a02ca0544a9381e2d221be2e3146cc3fca310f
jjklee <jjlee720@gmail.com>
2019-04-18 20:37:36 -0700

    Will make tests pass

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,9 +13,12 @@ var detectNetwork = function(cardNumber) {
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
-  var prefix = Number(cardNumber.slice(0, 2))
-  var prefix3 = Number(cardNumber.slice(0, 3))
-  var prefix4 = Number(cardNumber.slice(0, 4))
+  var prefix = Number(cardNumber.slice(0, 2));
+  var prefix3 = Number(cardNumber.slice(0, 3));
+  var prefix4 = Number(cardNumber.slice(0, 4));
+  var prefix6 = Number(cardNumber.slice(0, 6));
+  var chinaUnionPay = [624, 625, 626, 6282, 6283, 6284, 6285, 6286, 6287, 6288];
+  var switchNums = [4903, 4905, 4911, 4936, 6333, 6759, 564182, 633110];
 
   if(cardNumber.length === 14 && (prefix === 38 || prefix === 39)) {
   	return 'Diner\'s Club';
@@ -27,7 +30,11 @@ var detectNetwork = function(cardNumber) {
   	return 'Discover';
   } else if ((cardNumber.length >= 12 && cardNumber.length <=19) && (prefix4 === 5018 || prefix4 === 5020 || prefix4 === 5038 || prefix4 === 6304)) {
   	return 'Maestro';
+  } else if ((cardNumber.length === 16 || cardNumber.length === 18 || cardNumber.length === 19) && (switchNums.includes(prefix4) || switchNums.includes(prefix6))) {
+  	return "Switch";
   } else if ((cardNumber.length === 13 || cardNumber.length === 16 || cardNumber.length === 19) && cardNumber.startsWith('4')) {
   	return 'Visa';
-  }
+  } else if ((cardNumber.length >=16 && cardNumber.length <= 19) && (chinaUnionPay.includes(prefix3) || chinaUnionPay.includes(prefix4) || prefix6 >= 622126 && prefix6 <= 622925)) {
+  	return "China UnionPay";
+  } 
 };
